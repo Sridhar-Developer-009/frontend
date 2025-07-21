41a40ffba505c3fccd7b5c642c8448047cd58ac3
Sridhar-Developer-009 <sridharvenkatesan09@gmail.com>
2025-07-21 23:35:28 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Navbar() {
-  return <nav><h1>Navbar</h1></nav>;
-}
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>My Navbar</h1>
+    </nav>
+  );
+};
 
 export default Navbar;
